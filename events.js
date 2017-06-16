a572b2b507908b28beb7d691e560f70f3b862871
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-06-16 15:17:51 +0300

    Do not use es modules in events.js

--- events.js
+++ events.js
@@ -1,6 +1,6 @@
-export const init = command => `init:${command()}`
-export const prepare = command => `prepare:${command()}`
-export const start = command => `start:${command()}`
+exports.init = command => `init:${command()}`
+exports.prepare = command => `prepare:${command()}`
+exports.start = command => `start:${command()}`
 
-export const build = () => 'build'
-export const generator = () => 'generator'
+exports.build = () => 'build'
+exports.generator = () => 'generator'
