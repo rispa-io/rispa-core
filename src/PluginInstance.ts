71cd21888f54b66ee8d2381b4fa9f97c266f09fe
dvpetrov <dvpetrov.main@gmail.com>
2017-10-16 16:47:32 +0300

    Check context passed in constructor PluginInstance

--- src/PluginInstance.ts
+++ src/PluginInstance.ts
@@ -3,7 +3,11 @@ import { RispaContext } from './RispaContext'
 export default abstract class PluginInstance {
   context: RispaContext
 
-  constructor(context: RispaContext) {
+  constructor(context?: RispaContext) {
+    if (!context || !(context instanceof RispaContext)) {
+      throw new TypeError('`context` must be passed in super')
+    }
+
     this.context = context
   }
 
